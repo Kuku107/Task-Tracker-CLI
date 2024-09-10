6f9f21ba42426152486500fe2210ad9056ab81f6
Le Van Duc <leduc1078@gmail.com>
2024-09-10 17:49:56 +0700

    using framework Gson

--- src/main/java/com/learning/cli/utils/ReadJsonData.java
+++ src/main/java/com/learning/cli/utils/ReadJsonData.java
@@ -1,75 +1,35 @@
 package com.learning.cli.utils;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.learning.cli.model.Task;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.sql.Date;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
 import java.util.List;
+import java.nio.file.Path;
 
 public class ReadJsonData {
     public static List<Task> read() {
-        List<Task> tasks = new ArrayList<Task>();
-        String fileName = "src/main/java/com/learning/cli/data.json";
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            br.readLine();
-            String line;
-            int beginIndex;
-            int endIndex;
-            String value;
+        Path filePath =
+                Path.of("D:\\Coding\\Back-end project\\Task Tracker CLI\\src\\main\\java\\com\\learning\\cli\\data.json");
 
-            while (br.readLine().equals("{")) {
-                Task task = new Task();
-
-                // set Id
-                line = br.readLine();
-                beginIndex = line.indexOf("\": ") + 4;
-                endIndex = line.lastIndexOf("\"") - 1;
-                value = line.substring(beginIndex, endIndex + 1);
-                task.setId(Integer.parseInt(value));
-
-                // set Description
-                line = br.readLine();
-                beginIndex = line.indexOf("\": ") + 4;
-                endIndex = line.lastIndexOf("\"") - 1;
-                value = line.substring(beginIndex, endIndex + 1);
-                task.setDescription(value);
-
-                // set status
-                line = br.readLine();
-                beginIndex = line.indexOf("\": ") + 4;
-                endIndex = line.lastIndexOf("\"") - 1;
-                value = line.substring(beginIndex, endIndex + 1);
-                task.setStatus(value);
-
-                //set createdAt
-                line = br.readLine();
-                beginIndex = line.indexOf("\": ") + 4;
-                endIndex = line.lastIndexOf("\"") - 1;
-                value = line.substring(beginIndex, endIndex + 1);
-                task.setCreatedAt(Date.valueOf(value));
-
-                //set updateAt
-                line = br.readLine();
-                beginIndex = line.indexOf("\": ") + 4;
-                endIndex = line.lastIndexOf("\"") - 1;
-                value = line.substring(beginIndex, endIndex + 1);
-                task.setUpdatedAt(Date.valueOf(value));
-
-                tasks.add(task);
-                br.readLine();
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        String jsonList = null;
+        try {
+            jsonList = Files.readString(filePath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        return tasks;
+        Type listType =  new TypeToken<List<Task>>() {}.getType();
+        List<Task> task = new Gson().fromJson(jsonList, listType);
+        return task;
     }
 
     public static void main(String[] args) {
         List<Task> tasks = read();
-        for (Task task : tasks) {
-            System.out.println(task.toString());
+        for(Task task : tasks) {
+            System.out.println(task);
         }
     }
 }
--- src/main/java/com/learning/cli/utils/WriteJsonData.java
+++ src/main/java/com/learning/cli/utils/WriteJsonData.java
@@ -1,39 +1,35 @@
 package com.learning.cli.utils;
 
+import com.google.gson.Gson;
 import com.learning.cli.model.Task;
 
 import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Date;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class WriteJsonData {
     public static void write(List<Task> tasks) {
-        String fileName = "src/main/java/com/learning/cli/data.json";
-        StringBuilder jsonString = new StringBuilder();
-        jsonString.append("[\n");
-        for (Task task : tasks) {
-            jsonString.append("{\n");
-            jsonString.append("\"id\": \"").append(task.getId()).append("\",\n");
-            jsonString.append("\"description\": \"").append(task.getDescription()).append("\",\n");
-            jsonString.append("\"status\": \"").append(task.getStatus()).append("\",\n");
-            jsonString.append("\"createdAt\": \"").append(task.getCreatedAt().toString()).append("\",\n");
-            jsonString.append("\"updatedAt\": \"").append(task.getUpdatedAt().toString()).append("\"\n");
-            jsonString.append("},\n");
-        }
-        jsonString.deleteCharAt(jsonString.length() - 2);
-        jsonString.append("]");
-
-        try (FileWriter fw = new FileWriter(fileName)) {
-            fw.write(jsonString.toString());
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        String fileName = "D:\\Coding\\Back-end project\\Task Tracker CLI\\src\\main\\java\\com\\learning\\cli\\data.json";
+        Gson gson = new Gson();
+        try {
+            FileWriter fw = new FileWriter(fileName);
+            fw.write("[\n");
+            for(int i = 0; i < tasks.size() - 1; i++) {
+                fw.write(gson.toJson(tasks.get(i)));
+                fw.write(",\n");
+            }
+            fw.write(gson.toJson(tasks.get(tasks.size() - 1)) + "\n");
+            fw.write("]");
+            fw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public static void main(String[] args) {
+        String fileName = "D:\\Coding\\Back-end project\\Task Tracker CLI\\src\\main\\java\\com\\learning\\cli\\data.json";
         Task task1 = new Task(1, "hit the gym",
                 "todo", Date.valueOf("2024-08-31"), Date.valueOf("2024-08-31"));
         Task task2 = new Task(2, "hit the library",
